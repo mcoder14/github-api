e046a330827e70a8d934d3a86aa4ca860aa35778
Michael Byram <mbyram@mercurynewmedia.com>
2018-05-19 19:30:03 -0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchContainer from './containers/searchContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/searchContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Search from '../components/search';
+import UserBadge from '../components/userBadge';
+import RepoList from '../components/repoList'
+
+class SearchContainer extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Search />
+        <UserBadge />
+        <RepoList />
+      </div>
+    );
+  }
+}
+
+export default App;
